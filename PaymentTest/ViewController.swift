402cb795d96a6f3b96731d70e1419347852b0c1b
EGladun <gladunov.e.i.1.17@gmail.com>
2020-06-08 14:42:59 +0300

    Initial Commit

---
+++ PaymentTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentTest
+//
+//  Created by Egor on 08.06.2020.
+//  Copyright © 2020 Egor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
